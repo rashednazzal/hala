3e5c664256964ee2cc55b691aac27a3af560d912
rashednazzal <135705584+rashednazzal@users.noreply.github.com>
2023-06-06 14:16:01 +0300

    Update script.js

--- script.js
+++ script.js
@@ -11,8 +11,22 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
   document.getElementById("username").value = "";
   document.getElementById("password").value = "";
   
+  // Generate a random list of numbers
+  var randomNumbers = generateRandomNumbers(10);
+  console.log("Random Numbers:", randomNumbers);
+  
   // Display "thank you" message
   var thankYouMessage = document.createElement("p");
   thankYouMessage.textContent = "Thank you!";
   document.getElementById("loginForm").appendChild(thankYouMessage);
 });
+
+// Function to generate a random list of numbers
+function generateRandomNumbers(count) {
+  var numbers = [];
+  for (var i = 0; i < count; i++) {
+    var randomNumber = Math.floor(Math.random() * 100);
+    numbers.push(randomNumber);
+  }
+  return numbers;
+}
